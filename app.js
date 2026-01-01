93742af449200740e2244ba9248a3569560d1705
slls75966-try <slls75966@gmail.com>
2026-01-01 19:24:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,29 @@ if ('serviceWorker' in navigator) {
             .catch(err => console.log('Erreur SW :', err));
     });
 }
+
+let deferredPrompt;
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Empêche la barre par défaut de s'afficher
+  e.preventDefault();
+  // Stocke l'événement pour plus tard
+  deferredPrompt = e;
+  // Affiche notre bouton personnalisé
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    // Cache notre bouton
+    installBtn.style.display = 'none';
+    // Affiche la boîte de dialogue d'installation
+    deferredPrompt.prompt();
+    // Attend la réponse de l'utilisateur
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('L\'utilisateur a installé la PWA');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
